97c30fee8cb076ce84b93709f470ba9515c680f6
heinricov abraham <heinricoov@gmail.com>
2026-02-02 14:53:03 +0700

    tidy up storage

--- crud/src/modules/user/dto/update-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-
-/**
- * DTO untuk memvalidasi input saat update User.
- * Semua field dari CreateUserDto menjadi optional.
- */
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
